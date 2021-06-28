1102599e834f43abef2bb9107de76029d9f3a19e
Dmitrii <dp086@mail.ru>
2021-06-28 15:44:58 +0300

    добавил обработчики событий для полей "время въезда" и "время выезда", в которых вызывается функция для синхронизации значений этих полей

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { renderSimilarAds } from './similar_ads.js';
 import { createSimilarAd } from './data.js';
 import { disableForm, enableForm } from './form.js';
-import './validity.js';
+import './validation.js';
 
 const SIMILAR_AD_COUNT = 1;
 const similarAds = new Array(SIMILAR_AD_COUNT).fill('').map(() => createSimilarAd());
